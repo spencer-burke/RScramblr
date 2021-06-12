d1ecb72c572844b5414b35c8ddd277e2a6a4ee31
Spencer Burke <info.spencer.burke@gmail.com>
2021-06-11 22:45:16 -0700

    Adding a small amount to the autoloader to install parallel if needed(still needs work).

--- autoloader.sh
+++ autoloader.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 # This is a program which will set up all of the necessary tools and configurations for a REMNUX reverse engineering enbironment
+
+# install parallel if needed
+sudo apt install parallel
