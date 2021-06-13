4fea03e2d7a35475039c24b32d73fc62555ee87e
Spencer Burke <info.spencer.burke@gmail.com>
2021-06-12 22:04:04 -0700

    Improve and actually implement the running of the setup scripts

--- autoloader.sh
+++ autoloader.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This is a program which will set up all of the necessary tools and configurations for a REMNUX reverse engineering enbironment
+# This is a program which will set up all of the necessary tools and configurations for a REMNUX reverse engineering environment
 
-# install parallel if needed
-sudo apt install parallel
+# get all of the scripts to run
+ls /setup | xargs sh
